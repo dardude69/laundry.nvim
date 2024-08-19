2c5f5faab6bc340dbd7381dcb77c0a916b6697b0
Zak Stephens <ZAStephens@powercor.com.au>
2024-08-19 10:59:00 +1000

    Update Treesitter, define file extensions for searching for JavaScript

--- lua/laundry/vanilla.lua
+++ lua/laundry/vanilla.lua
@@ -52,10 +52,15 @@ vim.opt.expandtab = true -- <TAB> inserts spaces
 vim.opt.shiftwidth = 4 -- Width of shifting commands (i.e. << and >>)
 vim.opt.softtabstop = -1 -- Width of an indentation: -1 means replicate shiftwidth
 
--- Two-space indentation for JavaScript
+-- Special handling
+
 vim.api.nvim_create_autocmd('FileType', {
     pattern = 'javascript',
     callback = function()
+        -- Two-space indentation for JavaScript
         vim.opt_local.shiftwidth = 2
+
+        -- Suffixes to append to files while searching for them with the "gf" (Goto File) command.
+        vim.opt_local.suffixesadd = '.js,.json,.jsx,.mjs,.ts,.tsx'
     end,
 })
